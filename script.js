34a559fe075be2f85f83d7f4fb34f50f170408b4
kartik132000 <sujeetkumarsen37@gmail.com>
2025-03-02 08:56:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,22 +1,21 @@
 function createTable() {
-    //Write your code here
-	let  = window.prompt("Input number of rows");
-	if( === null || .trim() === '' || isNaN(rn) || Number (rn) <=0 ||) return;
+    let rn = window.prompt("Input number of rows");
+    if (rn === null || rn.trim() === "" || isNaN(rn) || Number(rn) <= 0) return;
 
-	let rn = window.prompt("Input number of rows");
-	if(cn === null || cn.trim() === '' || isNaN(cn) || Number (cn) <=0 ||) return;
+    let cn = window.prompt("Input number of columns");
+    if (cn === null || cn.trim() === "" || isNaN(cn) || Number(cn) <= 0) return;
 
-	rn= Number(rn);
-	cn= Number(cn);
+    rn = Number(rn);
+    cn = Number(cn);
+    
+    let table = document.getElementById("myTable");
+    table.innerHTML = "";
 
-	let table = document.getElementById("myTable");
-	table.innerHTML = "";
-	for(let i=0; i < rn; i++){
-		let row = table.insertRow();
-		for(let j =0; j < cn; j++){
-		let cell = row.insertcell();
-			cell.textContent = `Row-${i} column-${j}`;
-       }
-	}
+    for (let i = 0; i < rn; i++) {
+        let row = table.insertRow();
+        for (let j = 0; j < cn; j++) {
+            let cell = row.insertCell();
+            cell.textContent = Row-${i} Column-${j};
+        }
+    }
 }
-
